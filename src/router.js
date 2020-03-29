59b4ded912ae761d6230b2d44728744cff945a56
AndersonAndrad <Anderson_Andrade_@outlook.com>
2020-03-29 13:40:46 -0300

    {update: adding incidents list and delete, create session and information from only one ONG

--- src/router.js
+++ src/router.js
@@ -5,7 +5,8 @@ import {Router} from 'express';
   // your code is here ex: import User from './controllers/User.Controller.js'
   import Ong from './app/controllers/ongController';
   import Incident from './app/controllers/incidentController';
-
+  import Profile from './app/controllers/ProfileController';
+  import Session from './app/controllers/SessionController';
 // middlewares
   // your code is here ex: import User from './middlewares/Auth.middlewares.js'
 
@@ -15,8 +16,15 @@ router.get('/test', (request, response) => {
   return response.status(200).json({Status: 'Your application is running'});
 });
 
+router.post('/sessions', Session.create);
+
 router.post('/ongs', Ong.store);
 router.get('/ongs', Ong.index);
 
 router.post('/incidents', Incident.store);
+router.get('/incidents', Incident.index);
+router.delete('/incidents/:id', Incident.delete);
+
+router.get('/profile', Profile.index);
+
 export default router;
