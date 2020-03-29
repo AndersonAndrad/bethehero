97b372bc96a34d4ed93cd9530dfa827223006b08
AndersonAndrad <Anderson_Andrade_@outlook.com>
2020-03-28 22:22:14 -0300

    {update: adding router to post and get for controller Ong, and method post for controller incident}

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import {Router} from 'express';
 
 // controllers
   // your code is here ex: import User from './controllers/User.Controller.js'
+  import Ong from './app/controllers/ongController';
+  import Incident from './app/controllers/incidentController';
 
 // middlewares
   // your code is here ex: import User from './middlewares/Auth.middlewares.js'
@@ -11,6 +13,10 @@ const router = Router();
 
 router.get('/test', (request, response) => {
   return response.status(200).json({Status: 'Your application is running'});
-})
+});
 
+router.post('/ongs', Ong.store);
+router.get('/ongs', Ong.index);
+
+router.post('/incidents', Incident.store);
 export default router;
