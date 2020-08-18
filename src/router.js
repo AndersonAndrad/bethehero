3c39665e330abf1cbf53042d823ed5743a110bd0
_Anderson_Andrade_ <31743641+AndersonAndrad@users.noreply.github.com>
2020-08-18 01:00:26 -0300

    Add answer to api

--- src/router.js
+++ src/router.js
@@ -1,19 +1,31 @@
 // dependencies
-import {Router} from 'express';
+import { Router } from 'express';
 
 // controllers
-  // your code is here ex: import User from './controllers/User.Controller.js'
-  import Ong from './app/controllers/ongController';
-  import Incident from './app/controllers/incidentController';
-  import Profile from './app/controllers/ProfileController';
-  import Session from './app/controllers/SessionController';
+// your code is here ex: import User from './controllers/User.Controller.js'
+import Ong from './app/controllers/ongController';
+import Incident from './app/controllers/incidentController';
+import Profile from './app/controllers/ProfileController';
+import Session from './app/controllers/SessionController';
 // middlewares
-  // your code is here ex: import User from './middlewares/Auth.middlewares.js'
+// your code is here ex: import User from './middlewares/Auth.middlewares.js'
 
 const router = Router();
 
+router.get('/', (request, response) => {
+  return response.status(200).json({
+    Api_Version: 'v1',
+    Status_server: 'Online',
+    Documentation_Api: 'https://github.com/AndersonAndrad/betheheroBackend',
+    Frontend_Aplication: 'https://be-the-hero-we.herokuapp.com/',
+    Developer_API: ['Anderson Andrade'],
+    Developer_linkedin: ['https://www.linkedin.com/in/andersonandrad/'],
+    End: 'Thanks you for using my API',
+  });
+});
+
 router.get('/test', (request, response) => {
-  return response.status(200).json({Status: 'Your application is running'});
+  return response.status(200).json({ Status: 'Your application is running' });
 });
 
 router.post('/sessions', Session.create);
